340842b5c0f557df56f1234b8b75d92870498067
Emil Martinez <emilmrtnz@gmail.com>
2019-03-01 14:45:34 -0500

    updates

--- eclipse-workspace/MentoringInJava/src/Earth/Array.java
+++ eclipse-workspace/MentoringInJava/src/Earth/Array.java
@@ -1,5 +1,42 @@
 package Earth;
 
 public class Array {
-
+	
 }
+  
+	
+	// data type (int, strings, double...) - [] - variable name - equal operator 
+	int [] arr = new int [10]; 
+	
+	//data type -
+	int arr2 [] = new int [12];
+	
+	//data type - name - [] - equal operators {} - ;
+	int arr3[] = {11, 12, 25, 78, 46, 50};
+	
+	int[] arr4 = new int[6];
+	arr4[0] = 785;
+	arr4[1] = 14;
+	arr4[2] = 245;
+	arr4[3] = 87;
+	arr4[4] = 95;
+	arr4[5] = 
+			
+	String [] name = new String [4];	
+	
+	name [0] = "Naim";
+	name [1] = "Arif";
+	name [2] = "Shosh";
+    name [3] = "Shakib";
+    name [4] = "Urooj";
+    
+    // four different kinds of arrays
+    
+    // loops
+    // for keyword - parameter - body {}
+    //for(){};
+    for(int x; x < 10; x++) { 
+    System.out.println(x);
+    
+    	
+]
